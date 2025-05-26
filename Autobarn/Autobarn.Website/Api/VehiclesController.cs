02eca4b13616618defc2b051a98d953d69fa8227
Dylan Beattie <dylan@dylanbeattie.net>
2025-05-26 11:24:43 +0200

    API client creates random vehicles

--- Autobarn/Autobarn.Website/Api/VehiclesController.cs
+++ Autobarn/Autobarn.Website/Api/VehiclesController.cs
@@ -1,16 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Autobarn.Data;
 using Autobarn.Data.Entities;
 using Autobarn.Website.Models;
 
-namespace Autobarn.Website.Api
-{
+namespace Autobarn.Website.Api {
 	[Route("api/[controller]")]
 	[ApiController]
-	public class VehiclesController(AutobarnDbContext db)
-		: ControllerBase
-	{
+	public class VehiclesController(AutobarnDbContext db, ILogger<VehiclesController> logger)
+		: ControllerBase {
 		// GET: api/Vehicles
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<Vehicle>>> GetVehicles()
@@ -21,8 +19,7 @@ namespace Autobarn.Website.Api
 
 		// GET: api/Vehicles/5
 		[HttpGet("{id}")]
-		public async Task<ActionResult<Vehicle>> GetVehicle(string id)
-		{
+		public async Task<ActionResult<Vehicle>> GetVehicle(string id) {
 			var vehicle = await db.Vehicles
 				.Include(v => v.Model)
 				.ThenInclude(m => m.Make)
@@ -34,27 +31,20 @@ namespace Autobarn.Website.Api
 		// PUT: api/Vehicles/5
 		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
 		[HttpPut("{id}")]
-		public async Task<IActionResult> PutVehicle(string id, Vehicle vehicle)
-		{
-			if (id != vehicle.Registration)
-			{
+		public async Task<IActionResult> PutVehicle(string id, Vehicle vehicle) {
+			if (id != vehicle.Registration) {
 				return BadRequest();
 			}
 
 			db.Entry(vehicle).State = EntityState.Modified;
 
-			try
-			{
+			try {
 				await db.SaveChangesAsync();
 			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!VehicleExists(id))
-				{
+			catch (DbUpdateConcurrencyException) {
+				if (!VehicleExists(id)) {
 					return NotFound();
-				}
-				else
-				{
+				} else {
 					throw;
 				}
 			}
@@ -65,13 +55,11 @@ namespace Autobarn.Website.Api
 		// POST: api/Vehicles
 		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
 		[HttpPost]
-		public async Task<ActionResult<Vehicle>> PostVehicle(VehicleDto dto)
-		{
+		public async Task<ActionResult<Vehicle>> PostVehicle(VehicleDto dto) {
 			var model = await db.Models.FirstOrDefaultAsync(m => m.Code == dto.ModelCode);
 			if (model == null) return BadRequest($"Sorry, we don't have a car called {dto.ModelCode}");
 
-			var vehicle = new Vehicle
-			{
+			var vehicle = new Vehicle {
 				Model = model,
 				Registration = dto.Registration,
 				Year = dto.Year,
@@ -79,25 +67,22 @@ namespace Autobarn.Website.Api
 			};
 
 			db.Vehicles.Add(vehicle);
-			try
-			{
+			try {
 				await db.SaveChangesAsync();
 			}
-			catch (DbUpdateException)
-			{
+			catch (DbUpdateException) {
 				if (VehicleExists(vehicle.Registration!)) return Conflict();
 				throw;
 			}
+			logger.LogInformation($"Created vehicle: {vehicle}");
 			return CreatedAtAction("GetVehicle", new { id = vehicle.Registration }, vehicle);
 		}
 
 		// DELETE: api/Vehicles/5
 		[HttpDelete("{id}")]
-		public async Task<IActionResult> DeleteVehicle(string id)
-		{
+		public async Task<IActionResult> DeleteVehicle(string id) {
 			var vehicle = await db.Vehicles.FindAsync(id);
-			if (vehicle == null)
-			{
+			if (vehicle == null) {
 				return NotFound();
 			}
 
@@ -107,8 +92,7 @@ namespace Autobarn.Website.Api
 			return NoContent();
 		}
 
-		private bool VehicleExists(string id)
-		{
+		private bool VehicleExists(string id) {
 			return db.Vehicles.Any(e => e.Registration == id);
 		}
 	}
