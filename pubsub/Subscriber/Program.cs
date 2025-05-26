e714fc6ce7c02a90acc5e60af6fb0594f6dac266
Dylan Beattie <dylan@dylanbeattie.net>
2025-05-26 13:01:28 +0200

    Clean up code formatting and API controller code

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -4,7 +4,7 @@ using Messages;
 Console.WriteLine("Welcome to the subscriber!");
 var amqp = "amqps://csrsgicq:7PAjB9tTm2zcajhlCPr4y3uxqtGvXPEV@technical-crimson-pony.rmq6.cloudamqp.com/csrsgicq";
 var bus = RabbitHutch.CreateBus(amqp);
-const string SUBSCRIBER_ID = "techorama";
+const string SUBSCRIBER_ID = "dylan";
 await bus.PubSub.SubscribeAsync<Greeting>(SUBSCRIBER_ID, message => {
 	Console.WriteLine(message);
 });
